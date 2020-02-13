88d60acd559144451b2a7d57ce9aecc031485fd0
tanduong <tanduong@live.com>
2020-02-13 07:11:54 -0600

    PLAT 1223

--- remove.sh
+++ remove.sh
@@ -16,11 +16,21 @@ source blueprint-lib/init.sh
 # APP_NAME is the name of the Django app that will be modified
 ##
 
-BLUEPRINT="ProfilesBlueprint"
-NAME="Profiles"
+BLUEPRINT="$3"
+NAME="$4"
+
+if [ -z "$BLUEPRINT" ]
+then
+    BLUEPRINT="ProfilesBlueprint"
+fi
+
+if [ -z "$NAME" ] 
+then
+    NAME="Profiles"
+fi
 
 DATA_1="{ name: '${BLUEPRINT}', human_name: '${NAME}', access_route: '${BLUEPRINT}'},"
-DATA_2="import { ${BLUEPRINT}Navigator } from '..\/features\/${BLUEPRINT}\/navigator';"
+DATA_2="import ${BLUEPRINT}Navigator from '..\/features\/${BLUEPRINT}\/navigator';"
 DATA_3="${BLUEPRINT}: { screen: ${BLUEPRINT}Navigator },"
 
 echo ">> remove blueprint folder"
